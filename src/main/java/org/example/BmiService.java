a227bd4ecbb47a302765b18c4c5489b8c7be3dcd
xxmalikovxx <malikovtester@gmail.com>
2023-08-16 15:45:33 +0300

    add new src

--- src/main/java/org/example/BmiService.java
+++ src/main/java/org/example/BmiService.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class BmiService {
-    public int calculate(double heightInMeters, int weightInKg) {
+    public int calculate(double heightInMeters, double weightInKg) {
         double bmi = weightInKg / heightInMeters / heightInMeters;
         return (int) bmi;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weightInkg = 98;
-        int heightInMeters = (int) 1.87;
-        double bmi = service.calculate(weightInkg, heightInMeters);
+        double weightInkg = 98;
+        double heightInMeters = 1.87;
+        double bmi = service.calculate(heightInMeters,weightInkg);
         System.out.println("индекс массы=" + bmi);
     }
 }
